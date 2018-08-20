65821e9fc6eaa75e3ff538a5033b0bc787224454
Shrinath Nimare <shrinath.nimare@gmail.com>
2018-08-20 13:43:38 +0530

    Works now

--- foxdown.sh
+++ foxdown.sh
@@ -16,11 +16,12 @@ while read CURRENT_CHAPTER
 do
 	echo Downloading "$CURRENT_CHAPTER"
 	wget -U="$USER_AGENT" -O current_chapter.html "$CURRENT_CHAPTER" #downloads the current chapter
-	grep -o ">"[0-9]*"<" current_chapter.html | uniq | grep -o -c "[0-9]*" >> page_list.txt # to obtain the number of pages in the chapter
-	 CURRENT_CHAPTER_BASE_URL = $(printf "$CURRENT_CHAPTER" | grep -o "fanfox.net/.*[^1.html]") # saves the base URL in variable
+	grep -o ">"[0-9]*"<" current_chapter.html | uniq | grep -o "[0-9]*" >> page_list.txt # to obtain the number of pages in the chapter
+	 CURRENT_CHAPTER_BASE_URL=$(printf "$CURRENT_CHAPTER" | grep -o "fanfox.net/.*[^1.html]") # saves the base URL in variable
+	 #echo $CURRENT_CHAPTER_BASE_URL
 	while read CURRENT_PAGE_NUMBER #loops over all the 
 	do
-		wget -U="$USER_AGENT" -O current_page.html "$CURRENT_CHAPTER_BASE_URL/$CURRENT_PAGE_NUMBER"
+		wget -U="$USER_AGENT" -O current_page.html "$CURRENT_CHAPTER_BASE_URL$CURRENT_PAGE_NUMBER.html"
 		wget -nc $(grep -o -m 1 a.fanfox.net/store/manga[^?]* current_page.html) # this is to get the image url from the page, and download it
 	done < page_list.txt
 
